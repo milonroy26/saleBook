f5dfd15b2abb58845860f55b6e2276932c7b4a30
milonroy26 <101338962+milonroy26@users.noreply.github.com>
2023-01-05 14:35:06 +0600

    featured section done

--- main.js
+++ main.js
@@ -70,6 +70,39 @@ var swiper = new Swiper(".books-slider", {
     },
   });
 
+//   next
+
+var swiper = new Swiper(".featured-slider", {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+
+      450: {
+        slidesPerView: 2,
+      },
+
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
+
 
 
 
